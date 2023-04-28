efd1bd76fd682cda8d4ecc6ece49396bd124fc3b
JustSlamThings <justslamthings@gmail.com>
2023-04-28 03:42:11 -0400

    FINALLY MULTIPLED THE NUMBERS IN AN ARRAY BY 3

--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,20 @@
+import numpy as np
 
+
+my_list = [2, 4, 6]
+
+result = [item * 3 for item in my_list]
+print(result) 
+# took a while to figure this out and had to get help on the internet. 
+
+# a = np.array([1,2,3,4])
+
+# def multiply3(arr):
+#     # result = 1
+#     for i in arr:
+#         result = [i] * 3  
+#     return result
+
+
+
+# print(multiply_elements_by_3(a))
